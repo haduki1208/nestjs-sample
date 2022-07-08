53fb38fcaa3db17663bf2601d0dabcc04cade01c
haduki1208 <suguru.takahashi@asia-quest.jp>
2022-07-08 15:49:22 +0900

    envファイル導入

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -3,11 +3,11 @@ import type { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectio
 
 export const options: MysqlConnectionOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'mysql',
-  database: 'test',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number.parseInt(process.env.DB_PORT || '3306'),
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'mysql',
+  database: process.env.DB_DATABASE || 'test',
   entities: [__dirname + '/entities/*{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
 };
